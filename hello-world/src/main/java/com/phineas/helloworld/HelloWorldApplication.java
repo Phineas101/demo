22b2456a8baf4fafe0445d86a099d19552a107ff
Phineas <zjf_0731@163.com>
2023-05-05 22:20:00 -0400

    feat:init

---
+++ hello-world/src/main/java/com/phineas/helloworld/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.phineas.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+}
